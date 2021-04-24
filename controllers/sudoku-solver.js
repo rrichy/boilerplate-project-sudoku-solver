03b994ae1e9de669fa18c528239fe70763f8d12e
rrichy <rasulrichy@gmail.com>
2021-04-24 08:23:13 +0800

    update
    functional tests to follow

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,22 +1,128 @@
+const REGION_START_INDEX = [ [0, 3, 6], [27, 30, 33], [54, 57, 60] ];
+
 class SudokuSolver {
 
   validate(puzzleString) {
+    if(!puzzleString.split('').every(char => '123456789.'.includes(char))) return { error: 'Invalid characters in puzzle' };
+    
+    if(puzzleString.length !== 81) return { "error": "Expected puzzle to be 81 characters long" };
+    
+    return true;
   }
 
-  checkRowPlacement(puzzleString, row, column, value) {
-
+  transformCoordinate(coordinate) {
+    coordinate = coordinate.toLowerCase();
+    return 9 * (coordinate.charCodeAt(0) - 'a'.charCodeAt()) + (coordinate.charCodeAt(1) - '1'.charCodeAt());
   }
 
-  checkColPlacement(puzzleString, row, column, value) {
+  checkRowPlacement(puzzleString, index, value) {
+    if(value === '.') return true;
 
+    const START = 9 * Math.floor(index / 9);
+    const WHOLE_ROW = puzzleString.slice(START, START + 9);
+    const ROW = WHOLE_ROW.slice(0, index % 9) + WHOLE_ROW.slice(1 + index % 9);
+
+    return !ROW.includes(value); // returns true if given value is valid in index
   }
 
-  checkRegionPlacement(puzzleString, row, column, value) {
+  checkColPlacement(puzzleString, index, value) {
+    if(value === '.') return true;
 
+    const COL = puzzleString.split('').filter((a, i) => i % 9 === index % 9 && i !== index);
+
+    return !COL.includes(value); // returns true if given value is valid in index
+  }
+
+  checkRegionPlacement(puzzleString, index, value) {
+    if(value === '.') return true;
+
+    const REGION_START = REGION_START_INDEX[Math.floor(index / 27)][Math.floor((index % 9) / 3)];
+    let region = '';
+    for(let i = 0; i < 3; i++){
+      for(let j = 0; j < 3; j++){
+        let k = (REGION_START + j) + 9 * i;
+        if(k !== index) region += puzzleString[k];
+      }
+    }
+
+    return !region.includes(value); // returns true if given value is valid in index
   }
 
   solve(puzzleString) {
-    
+    for(let i = 0; i < 81; i++) {
+      if([!this.checkRowPlacement(puzzleString, i, puzzleString[i]), !this.checkColPlacement(puzzleString, i, puzzleString[i]), !this.checkRegionPlacement(puzzleString, i, puzzleString[i])].some(a => a === true)) return { error: 'Puzzle cannot be solved' };
+    }
+
+    let puzzle = puzzleString.split('').map((val, i) => {
+      if(val.length === 1 && val !== '.') return val;
+
+      let possible = '';
+      for(let n = 1; n < 10; n++) {
+        let row = this.checkRowPlacement(puzzleString, i, n),
+          col = this.checkColPlacement(puzzleString, i, n),
+          region = this.checkRegionPlacement(puzzleString, i, n);
+
+        if([row, col, region].every(a => a === true)) possible += n;
+      }
+
+      return possible;
+    });
+
+    // console.log(puzzle);
+
+    let count, sub = JSON.stringify(puzzle);
+    for(count = 0;; count++){
+      for(let i = 0; i < 81; i++) {
+        let row_start = 9 * Math.floor(i / 9);
+        let region_start = REGION_START_INDEX[Math.floor(i / 27)][Math.floor((i % 9) / 3)];
+        
+        if(puzzle[i].length === 1) {
+          for(let x = 0; x < 9; x++) {
+            // row
+            if(row_start + x !== i) puzzle[row_start + x] = puzzle[row_start + x].split('').filter(a => a !== puzzle[i]).join('');
+            // col
+            if(9 * x + (i % 9) !== i) puzzle[9 * x + (i % 9)] = puzzle[9 * x + (i % 9)].split('').filter(a => a !== puzzle[i]).join('');
+            // region
+            if((region_start + 9 * Math.floor(x / 3)) + x % 3 !== i) puzzle[(region_start + 9 * Math.floor(x / 3)) + x % 3].split('').filter(a => a !== puzzle[i]).join('');
+          }
+        }
+        else {
+          let possible = puzzle[i].split('');
+
+          let row = puzzle.slice(row_start, i % 9).concat(puzzle.slice(1 + i % 9)).join(''); // row, excluding the current index, i
+          let col = puzzle.filter((a, ind) => ind % 9 === i % 9 && ind !== i).join(''); // col, excluding the current index, i
+          let region = '';  // region, excluding the current index, i
+          for(let x = 0; x < 3; x++){
+            for(let j = 0; j < 3; j++){
+              let k = (region_start + j) + 9 * x;
+              if(k !== i) region += puzzle[k];
+            }
+          }
+
+          possible.forEach(pos => {
+            if(!row.includes(pos)) {
+              puzzle[i] = pos;
+              return;
+            }
+            if(!col.includes(pos)) {
+              puzzle[i] = pos;
+              return;
+            }
+            if(!region.includes(pos)) {
+              puzzle[i] = pos;
+              return;
+            }
+          });
+        }
+      }
+
+      if(JSON.stringify(puzzle) === sub) break;
+      else sub = JSON.stringify(puzzle);
+    }
+
+    console.log('solved at count: ' + count);
+    console.log(puzzle.join(''));
+    return puzzle.join('');
   }
 }
 
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,9 +1,68 @@
 const chai = require('chai');
 const assert = chai.assert;
 
+const { puzzlesAndSolutions } = require('../controllers/puzzle-strings.js');
+const defPuzzle = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
+const invPuzzle = '..99.5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
 const Solver = require('../controllers/sudoku-solver.js');
-let solver;
+let solver = new Solver();
 
 suite('UnitTests', () => {
+  test('Logic handles a valid puzzle string of 81 characters', () => {
+    puzzlesAndSolutions.map(a => a[0]).forEach(a => {
+      assert.isTrue(solver.validate(a), 'valid puzzle must return true');
+    });
+  });
 
-});
+  test('Logic handles a puzzle string with invalid characters (not 1-9 or .)', () => {
+    puzzlesAndSolutions.map(a => a[0]).forEach(a => {
+      assert.deepEqual(solver.validate(a.replace('.', 'x')), { error: 'Invalid characters in puzzle' });
+    });
+  });
+
+  test('Logic handles a puzzle string that is not 81 characters in length', () => {
+    puzzlesAndSolutions.map(a => a[0]).forEach(a => {
+      assert.deepEqual(solver.validate(a.slice(0, -1)), { error: 'Expected puzzle to be 81 characters long' });
+    });
+  });
+
+  test('Logic handles a valid row placement', () => {
+    assert.isTrue(solver.checkRowPlacement(defPuzzle, solver.transformCoordinate('A2'), '6'), 'valid row placement must return true');
+  });
+
+  test('Logic handles an invalid row placement', () => {
+    assert.isFalse(solver.checkRowPlacement(defPuzzle, solver.transformCoordinate('A2'), '1'), 'invalid row placement must return false');
+  });
+
+  test('Logic handles a valid column placement', () => {
+    assert.isTrue(solver.checkColPlacement(defPuzzle, solver.transformCoordinate('A2'), '6'), 'valid column placement must return true');
+  });
+
+  test('Logic handles an invalid column placement', () => {
+    assert.isFalse(solver.checkColPlacement(defPuzzle, solver.transformCoordinate('A2'), '2'), 'invalid column placement must return false');
+  });
+
+  test('Logic handles a valid region (3x3 grid) placement', () => {
+    assert.isTrue(solver.checkRegionPlacement(defPuzzle, solver.transformCoordinate('A2'), '6'), 'valid region placement must return true');
+  });
+
+  test('Logic handles an invalid region (3x3 grid) placement', () => {
+    assert.isFalse(solver.checkRegionPlacement(defPuzzle, solver.transformCoordinate('A2'), '2'), 'invalid region placement must return false');
+  });
+
+  test('Valid puzzle strings pass the solver', () => {
+    puzzlesAndSolutions.forEach(a => {
+      assert.isString(solver.solve(a[0]), 'valid puzzle must pass the solver');
+    });
+  });
+
+  test('Invalid puzzle strings fail the solver', () => {
+    assert.deepEqual(solver.solve(invPuzzle), { error: 'Puzzle cannot be solved' });
+  });
+
+  test('Solver returns the the expected solution for an incomplete puzzle', () => {
+    puzzlesAndSolutions.forEach(a => {
+      assert.equal(solver.solve(a[0]), a[1], 'incomplete but valid puzzles must return the solution');
+    });
+  });
+})
